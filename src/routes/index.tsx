b45788ab3081bbfb86f4b3846579d7118ae8cb70
Ntale Samad <swamaduntale98@gmail.com>
2026-05-13 00:41:55 +0300

    delete home component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
 import { CTABanner, Footer } from "@/components/landing-page/cta-and-footer";
 import { FeaturesSection } from "@/components/landing-page/FeaturesSection";
@@ -7,13 +7,8 @@ import HowItWorksSection from "@/components/landing-page/how-it-works-section";
 import NavBar from "@/components/landing-page/nav-bar";
 import { StatsSection } from "@/components/landing-page/stats-section";
 import { TestimonialsSection } from "@/components/landing-page/TestimonialsSection";
-import { useAuthStore } from "@/store/auth-store";
 
 export const Route = createFileRoute("/")({
-  beforeLoad: () => {
-    const { isAuthenticated } = useAuthStore.getState();
-    if (isAuthenticated()) throw redirect({ to: "/home" });
-  },
   component: HomeComponent,
 });
 
