8d20a2049780e37e677abf7dca42abfbf89f30f2
Ntale Samad <swamaduntale98@gmail.com>
2026-05-12 22:00:37 +0300

    🚀 lazy load chatbot to prevent page interaptions

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,19 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { lazy, Suspense } from "react";
 
-import { SokoWebchat } from "@/components/common/soko-webchat";
 import { PWAUpdateBanner } from "@/components/common/pwa-update-banner";
 import { ThemeProvider } from "@/components/theme-provider";
+
+const SokoWebchat = lazy(() =>
+  import("@/components/common/soko-webchat").then((m) => ({ default: m.SokoWebchat }))
+);
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
+      staleTime: 1000 * 60 * 5,
       retry: 1,
     },
   },
@@ -24,8 +28,9 @@ function RootComponent() {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <Outlet />
-        <SokoWebchat />
-        <TanStackRouterDevtools position="bottom-left" />
+        <Suspense>
+          <SokoWebchat />
+        </Suspense>
         <PWAUpdateBanner />
         <TanStackRouterDevtools position="bottom-right" />
       </ThemeProvider>
